4d410a8f48d77622056be3bdf3482ff6f6a4fa16
YuanQinHongXin <135498572+YuanQinHongXin@users.noreply.github.com>
2024-01-12 22:43:30 +0800

    Update FocusTimer.py

--- FocusTimer.py
+++ FocusTimer.py
@@ -1,33 +1,41 @@
 import time
+import winsound
 
-def countdown(minutes):
-    seconds = minutes * 60
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5 
+LONG_BREAK_MIN = 15
+WORK_SESSIONS_BEFORE_LONG_BREAK = 3
+
+session_count = 0
+while True: 
+    session_count += 1
+    session_complete = work_timer(WORK_MIN)
+    if session_complete:
+        if session_count % WORK_SESSIONS_BEFORE_LONG_BREAK == 0:
+            session_complete = break_timer(LONG_BREAK_MIN)
+        else:
+            session_complete = break_timer(SHORT_BREAK_MIN)
+
+def work_timer(work_minutes):
+    print('Work Time (' + str(work_minutes) + ' minutes):')
+    return countdown(work_minutes * 60)
+  
+def break_timer(break_minutes):  
+    print('Break Time (' + str(break_minutes) + ' minutes):')
+    return countdown(break_minutes * 60)
+
+def countdown(seconds):
     while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print_remaining(seconds)
         time.sleep(1)
         seconds -= 1
+    beep()
+    return True
 
-    print("Time's up!")
-
-def start_pomodoro():
-    print("Pomodoro Timer - 25 minutes")
-    countdown(25)
-
-    print("Take a short break - 5 minutes")
-    countdown(5)
-
-    print("Pomodoro Timer - 25 minutes")
-    countdown(25)
-
-    print("Take a short break - 5 minutes")
-    countdown(5)
-
-    print("Pomodoro Timer - 25 minutes")
-    countdown(25)
-
-    print("Take a long break - 15 minutes")
-    countdown(15)
-
-start_pomodoro()
+def print_remaining(seconds):
+    print(seconds, 'seconds remaining', end="\r")
+  
+def beep():
+    freq = 800
+    duration = 1000
+    winsound.Beep(freq, duration)
